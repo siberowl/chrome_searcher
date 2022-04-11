81b08783375ba3f25cfd62c35f30977ea8bdcfc9
siberowl <kon.aoki.us@gmail.com>
2022-04-11 17:58:37 -0400

    cleaned up code

--- content_script.js
+++ content_script.js
@@ -1,21 +1,28 @@
 document.addEventListener("keypress", (event) => {
-  // check that an input field is not in focus
-  if (window.getComputedStyle(document.activeElement).cursor !== "text") {
-    // select all input boxes
-    let inputBoxes = document.querySelectorAll("input");
-    let searchBox = null;
-    // choose the upper most as the search bar
-    inputBoxes.forEach((inputBox) => {
-      if (inputBox.type === "text" || inputBox.type === "search") {
-        if (inputBox.offsetParent != null && inputBox.style.display != "none") {
-          if (searchBox === null || inputBox.clientTop < searchBox.clientTop) {
-            searchBox = inputBox;
+  // check if key is /
+  if (event.key === "/") {
+    // check that an input field is not in focus
+    if (window.getComputedStyle(document.activeElement).cursor !== "text") {
+      // select all input boxes
+      let inputBoxes = document.querySelectorAll("input");
+      let searchBox = null;
+      // choose the upper most as the search bar
+      inputBoxes.forEach((inputBox) => {
+        if (inputBox.type === "text" || inputBox.type === "search") {
+          if (
+            inputBox.offsetParent != null &&
+            inputBox.style.display != "none"
+          ) {
+            if (
+              searchBox === null ||
+              inputBox.clientTop < searchBox.clientTop
+            ) {
+              searchBox = inputBox;
+            }
           }
         }
-      }
-    });
-    if (document.activeElement && searchBox !== null) {
-      if (event.key === "/") {
+      });
+      if (searchBox !== null) {
         event.preventDefault();
         setTimeout(() => searchBox.focus(), 0);
       }
