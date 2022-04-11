40a10ac7d925ae879a8a7fe180c457adb0e8bdd9
siberowl <kon.aoki.us@gmail.com>
2022-04-11 17:40:32 -0400

    added support for div input fields

--- content_script.js
+++ content_script.js
@@ -1,25 +1,20 @@
 document.addEventListener("keypress", (event) => {
-  // select all input boxes
-  let inputBoxes = document.querySelectorAll("input");
-  let searchBox = null;
-  // choose the upper most as the search bar
-  inputBoxes.forEach((inputBox) => {
-    if (
-      inputBox.type === "text" ||
-      inputBox.type === "search" ||
-      inputBox.type === ""
-    ) {
-      if (inputBox.offsetParent != null && inputBox.style.display != "none") {
-        if (searchBox === null || inputBox.clientTop < searchBox.clientTop) {
-          searchBox = inputBox;
+  // check that an input field is not in focus
+  if (window.getComputedStyle(document.activeElement).cursor !== "text") {
+    // select all input boxes
+    let inputBoxes = document.querySelectorAll("input");
+    let searchBox = null;
+    // choose the upper most as the search bar
+    inputBoxes.forEach((inputBox) => {
+      if (inputBox.type === "text" || inputBox.type === "search") {
+        if (inputBox.offsetParent != null && inputBox.style.display != "none") {
+          if (searchBox === null || inputBox.clientTop < searchBox.clientTop) {
+            searchBox = inputBox;
+          }
         }
       }
-    }
-  });
-  let inputs = ["input", "select", "button", "textarea"];
-  // check that an input field is not in focus
-  if (document.activeElement && searchBox !== null) {
-    if (!inputs.includes(document.activeElement.tagName.toLowerCase())) {
+    });
+    if (document.activeElement && searchBox !== null) {
       if (event.key === "/") {
         event.preventDefault();
         setTimeout(() => searchBox.focus(), 0);
