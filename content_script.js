4885176d91e9c6ba6afaf475ed5cc76976607703
Kon Aoki <kon.aoki.us@gmail.com>
2022-04-25 18:19:41 -0400

    refactored

--- content_script.js
+++ content_script.js
@@ -2,30 +2,36 @@ document.addEventListener("keypress", (event) => {
   // check if key is /
   if (event.key === "/") {
     // check that an input field is not in focus
-    if (window.getComputedStyle(document.activeElement).cursor !== "text") {
-      // select all input boxes
-      let inputBoxes = document.querySelectorAll("input");
-      let searchBox = null;
-      // choose the upper most as the search bar
-      inputBoxes.forEach((inputBox) => {
-        if (inputBox.type === "text" || inputBox.type === "search") {
-          if (
-            inputBox.offsetParent != null &&
-            inputBox.style.display != "none"
-          ) {
-            if (
-              searchBox === null ||
-              inputBox.clientTop < searchBox.clientTop
-            ) {
-              searchBox = inputBox;
-            }
-          }
-        }
-      });
-      if (searchBox !== null) {
-        event.preventDefault();
-        setTimeout(() => searchBox.focus(), 0);
+    if (document.activeElement) {
+      if (window.getComputedStyle(document.activeElement).cursor !== "text") {
+        focusOnSearchBox();
       }
     }
   }
 });
+
+let focusOnSearchBox = () => {
+  let searchBox = getSearchBox();
+  if (searchBox !== null) {
+    event.preventDefault();
+    // wait for blur to finish
+    setTimeout(() => searchBox.focus(), 0);
+  }
+};
+
+let getSearchBox = () => {
+  // select all input boxes
+  let inputBoxes = document.querySelectorAll("input");
+  let searchBox = null;
+  // choose the upper most as the search bar
+  inputBoxes.forEach((inputBox) => {
+    if (inputBox.type === "text" || inputBox.type === "search") {
+      if (inputBox.offsetParent != null && inputBox.style.display != "none") {
+        if (searchBox === null || inputBox.clientTop < searchBox.clientTop) {
+          searchBox = inputBox;
+        }
+      }
+    }
+  });
+  return searchBox;
+};
